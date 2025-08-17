12002cc4b3934838854acee50dda09e410892cc8
Alex <zykoleksandr08@gmail.com>
2025-08-17 18:05:17 +0300

    upd convert sec in date

--- HW_6.2.py
+++ HW_6.2.py
@@ -7,16 +7,19 @@ input_seconds = int(input("Enter seconds value: "))
 # input_seconds = 8_639_999
 # input_seconds = 8_640_001
 
-days = input_seconds // 60 ** 2 // 24
 
-if days % 10 == 1:
-    day_name = "день"
-elif days % 10 in [2, 3, 4,]:
-    day_name = "дні"
-else:
-    day_name = "днів"
 
 if 0 <= input_seconds <= 8_640_000:
+
+    days = input_seconds // 60 ** 2 // 24
+
+    if days % 10 == 1:
+        day_name = "день"
+    elif days % 10 in [2, 3, 4,]:
+        day_name = "дні"
+    else:
+        day_name = "днів"
+
     hours = str(input_seconds // 60 ** 2 % 24).zfill(2)
     minutes = str(input_seconds // 60 % 60).zfill(2)
     seconds = str(input_seconds % 60).zfill(2)
